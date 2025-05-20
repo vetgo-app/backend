1640a676c2594f446a2dd2e402667a866d96ee85
Florian <floriandelaunay75@gmail.com>
2025-05-20 12:11:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+const cors = require("cors"); // protextion flux back front
+app.use(cors());
+
+require("./models/connection"); // require pour la connexionn à la bdd
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
