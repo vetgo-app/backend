ed4ae7c0265cb161d11004e2bdb76765f82af9d9
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-05-22 17:03:05 +0200

    added the schema and route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var faqRouter = require("./routes/faq");
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/faq", faqRouter);
 
 module.exports = app;
