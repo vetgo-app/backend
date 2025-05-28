32313bd7f00c5bac1c7979b0d1274f9014c3ab85
Florian <floriandelaunay75@gmail.com>
2025-05-28 15:08:38 +0200

    d

--- routes/petDocuments.js
+++ routes/petDocuments.js
@@ -5,83 +5,102 @@ var router = express.Router();
 require("../models/connection");
 const mongoose = require("mongoose");
 
-
 const Pet = require("../models/pet");
-const uniqid = require('uniqid');
+const uniqid = require("uniqid");
 
 // Add the Cloudinary connection
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const User = require("../models/users");
 
-// Get the information from the FrontEnd 
-router.post('/', (req, res) => {
-    const {
-        newName: name, newRace: breed, newAge: age, newBirth: dateOfBirth, newWeight: weight, newSexe: sexe,
-        newIdentification: identification, newDocument: documents, newType: type, owner,
-    } = req.body.animalInfo;
+// Get the information from the FrontEnd
+router.post("/", (req, res) => {
+  const {
+    newName: name,
+    newRace: breed,
+    newAge: age,
+    newBirth: dateOfBirth,
+    newWeight: weight,
+    newSexe: sexe,
+    newIdentification: identification,
+    newDocument: documents,
+    newType: type,
+    token,
+  } = req.body.animalInfo;
 
+  User.findOne({ token })
+    .select("_id")
+    .then((user) => {
+      // Creation of the Animal
+      const newPet = new Pet({
+        name,
+        type,
+        breed,
+        age,
+        dateOfBirth,
+        weight,
+        sexe,
+        identification,
+        owner: user._id,
+        documents,
+      });
 
-    // Creation of the Animal
-    const newPet = new Pet({
-        name, type, breed, age, dateOfBirth, weight, sexe, identification, owner, documents
-    })
-
-    // Save the animal
-    newPet.save()
-        .then((data) => res.json({ result: "Animal ajouté", data }))
-
+      // Save the animal
+      newPet.save().then((data) => res.json({ result: "Animal ajouté", data }));
+    });
 });
 
-// Get the information from the pets collection 
-router.get('/:petId', (req, res) => {
-    Pet.findById(req.params.petId)
-        .then(data => {
-            if (!data) {
-                res.json({ result: false, error: "Pas d'animal trouvé" })
-            } else {
-                res.json({ result: true, petInfo: data })
-            }
-        })
+// Get the information from the pets collection
+router.get("/:petId", (req, res) => {
+  Pet.findById(req.params.petId).then((data) => {
+    if (!data) {
+      res.json({ result: false, error: "Pas d'animal trouvé" });
+    } else {
+      res.json({ result: true, petInfo: data });
+    }
+  });
 });
 
 // Getting the documents from the HealthJournal and Upload the document on Cloudninary
 router.post("/:petId", async (req, res) => {
-    if (!req.params.petId) {
-        res.json({ result: false, error: "Pas de params" });
+  if (!req.params.petId) {
+    res.json({ result: false, error: "Pas de params" });
+  }
+
+  // Move to temporary file
+  const docUniqueId = uniqid();
+  const documentPath = `./tmp/${docUniqueId}.pdf`;
+  const resultMove = await req.files.animalNewDocument.mv(documentPath);
+  const docName = req.files.animalNewDocument?.name;
+
+  if (!resultMove) {
+    // Sending the document to Cloudinary
+    const resultCloudinary = await cloudinary.uploader.upload(documentPath, {
+      public_id: docUniqueId,
+    });
+
+    if (resultCloudinary.secure_url) {
+      fs.unlinkSync(documentPath);
+
+      await Pet.updateOne(
+        { _id: req.params.petId },
+        {
+          $push: {
+            documents: {
+              uid: docUniqueId,
+              file: resultCloudinary.secure_url?.replace(".pdf", ".jpg"),
+              docName: docName,
+              date: new Date(),
+            },
+          },
+        }
+      );
     }
 
-    // Move to temporary file 
-    const docUniqueId = uniqid()
-    const documentPath = `./tmp/${docUniqueId}.pdf`;
-    const resultMove = await req.files.animalNewDocument.mv(documentPath);
-    const docName = req.files.animalNewDocument?.name;
-
-    if (!resultMove) {
-        // Sending the document to Cloudinary
-        const resultCloudinary = await cloudinary.uploader.upload(documentPath, { public_id: docUniqueId });
-
-        if (resultCloudinary.secure_url) {
-            fs.unlinkSync(documentPath);
-
-            await Pet.updateOne(
-                { _id: req.params.petId },
-                {
-                    $push: {
-                        "documents": {
-                            uid: docUniqueId,
-                            file: resultCloudinary.secure_url?.replace(".pdf", ".jpg"),
-                            docName: docName,
-                            date: new Date(),
-                        }
-                    }
-                }
-            )
-        }
-
-        res.json({ result: true, url: resultCloudinary.secure_url });
-    } else {
-        res.json({ result: false, error: resultMove });
-    };
+    res.json({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    res.json({ result: false, error: resultMove });
+  }
 });
 
 module.exports = router;
